6265b532abd811476ccb51b94c32883aa33b2890
Muhammed Hanifi Dikmen <muhadikmen@gmail.com>
2023-05-23 21:59:20 +0300

    Add project files.

---
+++ StringVsStringBuilder/Program.cs
@@ -0,0 +1,23 @@
+﻿//https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/strings/
+
+using System.Diagnostics;
+using System.Text;
+
+Stopwatch stopwatch = new Stopwatch();
+stopwatch.Start();
+string s1 = string.Empty;
+
+for (int i = 0; i < 100000; i++)
+{
+    s1 += "a";
+}
+stopwatch.Stop();
+Console.WriteLine($"String: {stopwatch.ElapsedMilliseconds} ms");
+stopwatch.Restart();
+StringBuilder s2 = new StringBuilder();
+for (int i = 0; i < 100000; i++)
+{
+    s2.Append("a");
+}
+stopwatch.Stop();
+Console.WriteLine($"StringBuilder: {stopwatch.ElapsedMilliseconds} ms");
